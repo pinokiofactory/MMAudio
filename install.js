5e8f5446b6d8f38ba4e8bab1105a19739077a84a
cocktailpeanut <cocktailpeanuts@proton.me>
2024-12-21 16:50:49 -0500

    update

--- install.js
+++ install.js
@@ -5,7 +5,7 @@ module.exports = {
       method: "shell.run",
       params: {
         message: [
-          "git clone https://github.com/hkchengrex/MMAudio.git app",
+          "git clone https://github.com/peanutcocktail/MMAudio.git app -b img2audio",
         ]
       }
     },
