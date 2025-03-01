b115e48e47bbf81c68b40fecc6e2de64241114cd
cocktailpeanut <cocktailpeanuts@proton.me>
2025-03-01 15:16:47 -0800

    use stable, nightly is broken

--- torch.js
+++ torch.js
@@ -37,7 +37,7 @@ module.exports = {
       "params": {
         "venv": "{{args && args.venv ? args.venv : null}}",
         "path": "{{args && args.path ? args.path : '.'}}",
-        "message": "uv pip install --pre torch torchvision torchaudio --index-url https://download.pytorch.org/whl/nightly/cpu"
+        "message": "uv pip install torch torchvision torchaudio"
       }
     },
     // linux nvidia
