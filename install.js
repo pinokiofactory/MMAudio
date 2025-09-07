343b727c2532c9c462378f3c2092223cdffa4518
6Morpheus6 <wanton-decay@gmx.net>
2025-09-07 21:22:27 +0200

    install torch after requirements

--- install.js
+++ install.js
@@ -10,18 +10,6 @@ module.exports = {
         ]
       }
     },
-    // Delete this step if your project does not use torch
-    {
-      method: "script.start",
-      params: {
-        uri: "torch.js",
-        params: {
-          venv: "env",                // Edit this to customize the venv folder path
-          path: "app",                // Edit this to customize the path to start the shell from
-          // xformers: true   // uncomment this line if your project requires xformers
-        }
-      }
-    },
     // Edit this step with your custom install commands
     {
       method: "shell.run",
@@ -32,6 +20,18 @@ module.exports = {
           "uv pip install -r pyproject.toml" // Uncomment the command below if new issues are introduced, but the pyproject.toml file must be updated
           // "uv pip install -r ../pyproject.toml" //change it back to just pyproject.toml once the original repo fixes their errors
         ]
+      }
+    },
+    // Delete this step if your project does not use torch
+    {
+      method: "script.start",
+      params: {
+        uri: "torch.js",
+        params: {
+          venv: "env",                // Edit this to customize the venv folder path
+          path: "app",                // Edit this to customize the path to start the shell from
+          // xformers: true   // uncomment this line if your project requires xformers
+        }
       }
     }
   ]
