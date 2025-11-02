de69987ec13348342e919047c33ddf4ed64cedb1
6Morpheus6 <wanton-decay@gmx.net>
2025-11-02 21:02:07 +0100

    pin huggingface_hub 0.36.0
    Fixes "TypeError: BigVGAN._from_pretrained() missing 2 required keyword-only arguments: 'proxies' and 'resume_download'"

--- install.js
+++ install.js
@@ -1,36 +1,32 @@
 module.exports = {
   run: [
-    // Edit this step to customize the git repository to use
     {
       method: "shell.run",
       params: {
         message: [
-          //"git clone https://github.com/{{platform === 'darwin' ? 'cocktailpeanut' : 'hkchengrex'}}/MMAudio.git app",
           "git clone https://github.com/hkchengrex/MMAudio.git app",
         ]
       }
     },
-    // Edit this step with your custom install commands
     {
       method: "shell.run",
       params: {
-        venv: "env",                // Edit this to customize the venv folder path
-        path: "app",                // Edit this to customize the path to start the shell from
+        venv: "env",
+        path: "app",
         message: [
-          "uv pip install -r pyproject.toml", // Uncomment the command below if new issues are introduced, but the pyproject.toml file must be updated
-          "uv pip install hf-xet"
+          "uv pip install -r pyproject.toml",
+          "uv pip install huggingface_hub==0.36.0 hf-xet"
         ]
       }
     },
-    // Delete this step if your project does not use torch
     {
       method: "script.start",
       params: {
         uri: "torch.js",
         params: {
-          venv: "env",                // Edit this to customize the venv folder path
-          path: "app",                // Edit this to customize the path to start the shell from
-          // xformers: true   // uncomment this line if your project requires xformers
+          venv: "env",
+          path: "app",
+          // xformers: true
         }
       }
     }
